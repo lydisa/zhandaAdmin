0c1d28fdb2ce25bdf9aaeb6d2cf514c81e6895b8
huangmeigui <385346894@qq.com>
2016-06-20 15:48:04 +0800

    表格测试

--- zhandaAdmin/zhandaAdmin-webapps/src/main/webapp/script.js
+++ zhandaAdmin/zhandaAdmin-webapps/src/main/webapp/script.js
@@ -1,5 +1,5 @@
 // script.js
-'use strict';
+// 'use strict';
 // create the module and name it scotchApp
 // also include ngRoute for all our routing needs
 angular.module('component', []);
@@ -91,3 +91,32 @@ scotchApp.controller('test', function ($scope,ProductService) {
     //     $scope.size = value;
     // });
 });
+
+for (var i=0; i<6; i++) {
+    var row = document.querySelector("table").insertRow(-1);
+    for (var j=0; j<6; j++) {
+        var letter = String.fromCharCode("A".charCodeAt(0)+j-1);
+        row.insertCell(-1).innerHTML = i&&j ? "" : i||letter;
+    }
+}
+var DATA={}, INPUTS=[].slice.call(document.querySelectorAll("input"));
+INPUTS.forEach(function(elm) {
+    elm.onfocus = function(e) {
+        e.target.value = localStorage[e.target.id] || "";
+    };
+    elm.onblur = function(e) {
+        localStorage[e.target.id] = e.target.value;
+        computeAll();
+    };
+    var getter = function() {
+        var value = localStorage[elm.id] || "";
+        if (value.charAt(0) == "=") {
+            with (DATA) return eval(value.substring(1));
+        } else { return isNaN(parseFloat(value)) ? value : parseFloat(value); }
+    };
+    Object.defineProperty(DATA, elm.id, {get:getter});
+    Object.defineProperty(DATA, elm.id.toLowerCase(), {get:getter});
+});
+(window.computeAll = function() {
+    INPUTS.forEach(function(elm) { try { elm.value = DATA[elm.id]; } catch(e) {} });
+})();
