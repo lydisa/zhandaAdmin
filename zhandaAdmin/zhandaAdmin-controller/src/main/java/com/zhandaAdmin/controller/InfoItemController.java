007cebff8b3278e6787f3a9850cbb4f332b0abe3
huangmeigui <385346894@qq.com>
2016-07-22 11:40:53 +0800

    动态加载4

--- zhandaAdmin/zhandaAdmin-controller/src/main/java/com/zhandaAdmin/controller/InfoItemController.java
+++ zhandaAdmin/zhandaAdmin-controller/src/main/java/com/zhandaAdmin/controller/InfoItemController.java
@@ -1,26 +1,21 @@
 package com.zhandaAdmin.controller;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.zhandaAdmin.common.dao.CommonResult;
-import com.zhandaAdmin.service.InfoDemo;
-//import org.apache.commons.beanutils.BeanUtils;
-import net.sf.json.groovy.GJson;
-import org.apache.commons.beanutils.BeanUtils;
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.zhandaAdmin.common.dao.CommonResult;
+import com.zhandaAdmin.service.InfoDemo;
+
+
 
 /**
  * Created by admin on 2016/7/22.
